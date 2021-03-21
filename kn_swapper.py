47a81b353be1491abfce41e07b8b584421c2e701
Holzer Matthias <wien.matthias.holzer@gmail.com>
2021-03-21 18:43:00 +0100

    Co-authored-by: Alwin Schuster <Alwinator@users.noreply.github.com> Co-authored-by: Constantin Winkler <Lucostus@users.noreply.github.com>

--- kn_swapper.py
+++ kn_swapper.py
@@ -1,16 +1,14 @@
 import re
 
-def reader():
-    with open("test.conf",'r') as f:
+
+def reader(path):
+    with open(path,'r') as f:
         txt = f.read()
 
     return txt
 
-def swapper_to_kn(txt,kn):
-    return txt.replace(kn, '?kn')
-
-def swapper(txt,kn):
-    return txt.replace('?kn',str(kn))
+def swapper(txt,kn_ser,kn_rep):
+    return txt.replace(kn_ser,str(kn_rep))
 
 def evller(txt, kn):
     findings = re.finditer(r'\?kn(\+|\-)[0-9]{1,3}', txt)
@@ -18,12 +16,31 @@ def evller(txt, kn):
         txt = txt.replace(found.group(0), str(eval(f"{kn}{found.group(0)[3:]}")))
     return txt
 
-def writer(txt, kn):
-    with open(f"conf{kn}.txt", 'w') as f:
+def writer(txt, path, kn):
+    with open(f"{kn}{path}", 'w') as f:
         f.write(txt)
 
 if __name__ == '__main__':
+    files = [
+        'lab15_r1.conf',
+        'lab15_r2.conf',
+        'lab15_r3.conf',
+    ]
+    kn_numbers = [9,29]
+    things = {
+    '?1kn': [74,9],
+    '?2kn': [180,119],
+    '?3kn': [115,223],
+    '?area': [10,15],
+    '?ospf-id': [14,4],
+    }
     
-    kn = [7,15,16,29]
-    for i in kn:
-        writer(swapper(evller(reader(),i),i),i)
+    for i in range(len(kn_numbers)):
+        for f in files:
+            txt = reader(f)
+            for k, v in things.items():
+                txt = swapper(txt,k,v[i])
+            writer(txt,f,kn_numbers[i])
+
+
+
